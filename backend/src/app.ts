6b681a39bf79eb9efcc89f20d1a6a4a6027d0ead
gsxhnd <gsxhnd@gmail.com>
2020-01-17 15:26:28 +0800

    update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,28 @@
 import koa from "koa";
+import koaRouter from "koa-router";
 import { logger } from "./middleware/test";
 
 const app = new koa();
+const router = new koaRouter();
 
 app.use(logger);
 
-app.use(async ctx => {
+app.use(async (ctx, next) => {
+  await next();
+  if (ctx.status === 404) {
+    ctx.status = 404;
+    ctx.body = "404";
+  }
+});
+
+router.get("/hello", async ctx => {
   ctx.body = "Hello World1";
 });
+
+router.get("/hello1", (ctx, next) => {
+  ctx.body = "Hello World1";
+});
+
+app.use(router.routes());
 
 app.listen(3000);
