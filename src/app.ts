e2ee5032865c2fb2229be6944672250edbe2cfc8
gsxhnd <gsxhnd@gmail.com>
2020-06-28 10:39:36 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import koa from "koa";
 import { AppRoutes } from "./routes";
 import { Render } from "./middleware/Render";
+import bodyParser from "koa-bodyparser";
 
 const app = new koa();
+app.use(bodyParser());
 app.use(AppRoutes.routes());
 app.use(Render);
 
-app.listen(3000);
+app.listen(8001);
--- src/routes.ts
+++ src/routes.ts
@@ -5,13 +5,6 @@ import koa from "koa";
 const hello = new Hello();
 const AppRoutes = new koaRouter();
 
-AppRoutes.get("/jav/star_list", hello.test);
-AppRoutes.get("/jav/star_detail/:star", hello.test);
-AppRoutes.get("/jav/tag", hello.test);
-AppRoutes.get("/jav/tag_detail/:tag", hello.test);
-AppRoutes.get("/jav/subject_list/:banko_prefix", hello.test);
-AppRoutes.get("/jav/subject_detail/:banko", hello.test);
-
-AppRoutes.get("/bangumi", hello.test);
+AppRoutes.get("/hello", hello.test);
 
 export { AppRoutes };
